ae82fab222e8a3d87a9a1aa012521e23000b35a9
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-08-26 18:51:16 -0700

    Added more decorators and some frontend stuff. Need to clean this up completely

--- pyjit/decorators.py
+++ pyjit/decorators.py
@@ -2,17 +2,68 @@ __author__ = 'sarangis'
 
 from pyjit.visitors import *
 from pyjit.type_infer import *
-from pyjit.pretty_print import *
+from pyjit.codegen import *
+
+def arg_pytype(arg):
+    if isinstance(arg, np.ndarray):
+        if arg.dtype == np.dtype('int32'):
+            return array(int32)
+        elif arg.dtype == np.dtype('int64'):
+            return array(int64)
+        elif arg.dtype == np.dtype('double'):
+            return array(double64)
+        elif arg.dtype == np.dtype('float'):
+            return array(float32)
+    elif isinstance(arg, int) & (arg < sys.maxint):
+        return int64
+    elif isinstance(arg, float):
+        return double64
+    else:
+        raise Exception("Type not supported: %s" % type(arg))
+
+def specialize(ast, infer_ty, mgu):
+    def _wrapper(*args):
+        return None
+        # types = map(arg_pytype, list(args))
+        # spec_ty = TFun(argtys=types, retty=TVar("$retty"))
+        # unifier = unify(infer_ty, spec_ty)
+        # specializer = compose(unifier, mgu)
+        #
+        # retty = apply(specializer, TVar("$retty"))
+        # argtys = [apply(specializer, ty) for ty in types]
+        # print('Specialized Function:', TFun(argtys, retty))
+        #
+        # if determined(retty) and all(map(determined, argtys)):
+        #     key = mangler(ast.fname, argtys)
+        #     # Don't recompile after we've specialized.
+        #     if key in function_cache:
+        #         return function_cache[key](*args)
+        #     else:
+        #         llfunc = codegen(ast, specializer, retty, argtys)
+        #         pyfunc = wrap_module(argtys, llfunc)
+        #         function_cache[key] = pyfunc
+        #         return pyfunc(*args)
+        # else:
+        #     raise UnderDeteremined()
+    return _wrapper
 
 def autojit(fn):
     transformer = PythonVisitor()
-    core = transformer(fn)
+    ast = transformer(fn)
+    (ty, mgu) = typeinfer(ast)
+    return specialize(ast, ty, mgu)
+
+def typeinfer(ast):
     infer = InferType()
-    sig = infer.visit(core)
-    print('Signature:%s \n' % sig)
+    ty = infer.visit(ast)
+    mgu = solve(infer.constraints)
+    infer_ty = apply(mgu, ty)
+    return (infer_ty, mgu)
 
-    print('Constraints:')
-    for (a,b) in infer.constraints:
-        print(a, '~', b)
-
-    print(pformat_ast(core))
+def codegen(ast, specializer, retty, argtys):
+    cgen = IREmitter(specializer, retty, argtys)
+    mod = cgen.visit(ast)
+    cgen.function.verify()
+    print(cgen.function)
+    # print(module.to_native_assembly())
+    return cgen.function
--- pyjit/type_infer.py
+++ pyjit/type_infer.py
@@ -101,3 +101,94 @@ class InferType(object):
 
     def generic_visit(self, node):
         raise NotImplementedError
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+from pyjit.nodes import *
+from collections import deque
+
+def empty():
+    return {}
+
+
+def apply(s, t):
+    if isinstance(t, ConstructorTy):
+        return t
+    elif isinstance(t, AppTy):
+        return AppTy(apply(s, t.a), apply(s, t.b))
+    elif isinstance(t, FuncTy):
+        argTys = [apply(s, a) for a in t.argTys]
+        retTy = apply(s, t.retTy)
+        return FuncTy(argTys, retTy)
+    elif isinstance(t, VarTy):
+        return s.get(t.s, t)
+
+def applyList(s, xs):
+    return [(apply(s, x), apply(s, y)) for x, y in xs]
+
+def unify(x, y):
+    if isinstance(x, AppTy) and isinstance(y, AppTy):
+        s1 = unify(x.a, y.a)
+        s2 = unify(apply(s1, x.b), apply(s1, y.b))
+        return compose(s2, s1)
+    elif isinstance(x, ConstructorTy) and isinstance(y, ConstructorTy) and (x == y):
+        return empty()
+    elif isinstance(x, FuncTy) and isinstance(y, FuncTy):
+        if (len(x.argTys) != len(y.argTys)):
+            return Exception("Wrong number of arguments")
+        s1 = solve(zip(x.argTys, y.argTys))
+        s2 = unify(apply(s1, x.retTy), apply(s1, y.retTy))
+        return compose(s2, s1)
+    elif isinstance(x, VarTy):
+        return bind(x.s, y)
+    elif isinstance(y, VarTy):
+        return bind(y.s, x)
+    else:
+        raise InferError
+
+def solve(xs):
+    mgu = empty()
+    cs = deque(xs)
+    while len(cs):
+        (a, b) = cs.pop()
+        s = unify(a, b)
+        mgu = compose(s, mgu)
+        cs = deque(applyList(s, cs))
+    return mgu
+
+def bind(n, x):
+    if x == n:
+        return empty()
+    elif occurs_check(n, x):
+        raise Exception("InfiniteType") #InfiniteType(n, x)
+    else:
+        return dict([(n, x)])
+
+def occurs_check(n, x):
+    return n in ftv(x)
+
+def union(s1, s2):
+    nenv = s1.copy()
+    nenv.update(s2)
+    return nenv
+
+def compose(s1, s2):
+    s3 = dict((t, apply(s1, u)) for t, u in s2.items())
+    return union(s1, s3)
+
+class UnderDeteremined(Exception):
+    def __str__(self):
+        return "The types in the function are not fully determined by the \
+                input types. Add annotations."
+
+class InferError(Exception):
+    def __init__(self, ty1, ty2):
+        self.ty1 = ty1
+        self.ty2 = ty2
+
+    def __str__(self):
+        return '\n'.join([
+            "Type mismatch: ",
+            "Given: ", "\t" + str(self.ty1),
+            "Expected: ", "\t" + str(self.ty2)
+        ])
